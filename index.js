5a90d14fe22f415f1cc1823b38edf35fb0bd31d0
Ahmed Wheida <ahmed.wheida@gmail.com>
2020-04-22 19:06:38 +0100

    clean up

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-// Setup basic express server
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var redis = require('socket.io-redis');
 var port = process.env.PORT || 3000;
-var serverName = process.env.NAME || 'Unknown';
-
-io.adapter(redis({ host: 'redis', port: 6379 }));
-
+var serverName = process.env.NAME || 'Live';
+const kubemq = require('kubemq-nodejs')
 
 
 server.listen(port, function () {
@@ -24,91 +19,39 @@ app.head('/health', function (req, res) {
   res.sendStatus(200);
 });
 
-// Chatroom
-
-var numUsers = 0;
-
-io.on('connection', function (socket) {
-  socket.emit('my-name-is', serverName);
-
-  var addedUser = false;
-
-  // when the client emits 'new message', this listens and executes
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
-    });
-  });
-
-  // when the client emits 'add user', this listens and executes
-  socket.on('add user', function (username) {
-    if (addedUser) return;
-
-    // we store the username in the socket session for this client
-    socket.username = username;
-    ++numUsers;
-    addedUser = true;
-    socket.emit('login', {
-      numUsers: numUsers
-    });
-    // echo globally (all clients) that a person has connected
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-      numUsers: numUsers
-    });
-  });
-
-  // when the client emits 'typing', we broadcast it to others
-  socket.on('typing', function () {
-    socket.broadcast.emit('typing', {
-      username: socket.username
-    });
-  });
-
-  // when the client emits 'stop typing', we broadcast it to others
-  socket.on('stop typing', function () {
-    socket.broadcast.emit('stop typing', {
-      username: socket.username
-    });
-  });
-
-  // when the user disconnects.. perform this
-  socket.on('disconnect', function () {
-    if (addedUser) {
-      --numUsers;
-
-      // echo globally that this client has left
-      socket.broadcast.emit('user left', {
-        username: socket.username,
-        numUsers: numUsers
+async function connect () {
+  let success = false
+  if (success === false) {
+    try {
+      let pub = await new kubemq.Publisher('kubemq-cluster-rest.kubemq.svc.cluster.localgit ', '9090', 'pub', 'testing_event_channel');
+     
+      let event = await new kubemq.Publisher.Event(kubemq.stringToByte('test'));
+      
+      await pub.send(event).then(res => {
+          console.log(res);
       });
+      
+    
+      let sub = await new kubemq.Subscriber('kubemq-cluster-rest.kubemq.svc.cluster.local', '9090', 'sub', 'testing_event_channel');
+      
+      await sub.subscribeToEvents(msg => {
+          console.log('msg:' + String.fromCharCode.apply(null, msg.Body))
+        }
+          , err => {
+          console.log('error:' + err)
+      });
+      success = true
+      
+    } catch (error) {
+      console.log(error)
+      await setTimeout(function() {
+        connect ()
+      }, 3000);
     }
-  });
-});
-
-
-
-/////
-const kubemq = require('kubemq-nodejs')
-let pub = new kubemq.Publisher('kubemq-cluster-rest.kubemq.svc.cluster.local', '9090', 'pub', 'testing_event_channel');
- 
-let event = new kubemq.Publisher.Event(kubemq.stringToByte('test'));
- 
-pub.send(event).then(res => {
-    console.log(res);
-});
- 
-
-let sub = new kubemq.Subscriber('kubemq-cluster-rest.kubemq.svc.cluster.local', '9090', 'sub', 'testing_event_channel');
- 
-sub.subscribeToEvents(msg => {
-    console.log('msg:' + String.fromCharCode.apply(null, msg.Body))
   }
-    , err => {
-    console.log('error:' + err)
-});
+  
+}
 
 
+connect()
 
